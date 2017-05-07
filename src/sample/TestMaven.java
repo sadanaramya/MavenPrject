6c06707351d5fbc57a69aa3793d560c9f7856f10
pridevar <pridevar@PRIDEVAR-FX1B9.cisco.com>
2017-05-07 08:30:11 +0530

    modified class

--- src/sample/TestMaven.java
+++ src/sample/TestMaven.java
@@ -4,14 +4,7 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class TestMaven {
-	/*public static void main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Run as -> clean and Run as -> Install");
-	}*/
-	
-	@Test
-	public void testMaven()
-	{
-		Reporter.log("Running the Maven Project using TestNg",true);
 	}
-
 }
